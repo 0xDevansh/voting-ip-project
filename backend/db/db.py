df0245dd71edfeb56f9805dead5d4d41b4cb5eb4
DeathVenom54 <fdragon602@gmail.com>
2023-07-09 14:51:46 +0530

    add methods to register voters and candidates

--- backend/db/db.py
+++ backend/db/db.py
@@ -50,13 +50,55 @@ class Database(metaclass=Singleton):
 
     def create_poll(self, name, type, description=None):
         cursor = self.conn.cursor()
-        # check if a poll with same name exists
+        # input validation
+        if type not in ['runoff', 'fptp', 'approval', 'referendum']:
+            raise Exception(f'Invalid type: {type}')
         cursor.execute('SELECT id FROM polls WHERE name = ?', (name,))
         if cursor.fetchone():
-            raise Exception('Poll already exists')
+            raise Exception('Poll with the same name already exists')
 
         cursor.execute('INSERT INTO polls (name, type, description) VALUES (?, ?, ?)', (name, type, description))
         cursor.close()
         self.conn.commit()
 
-#    def register_voter(self):
+    def register_voters(self, poll_id, voters):
+        cursor = self.conn.cursor()
+        # validate voters
+        voter_ids = []
+        cursor.execute('SELECT voter_id FROM poll_voter WHERE poll_id = ?', (poll_id,))
+        existing_voters = cursor.fetchall()
+        existing_voters = [v[0] for v in existing_voters]
+        records = []
+        for voter in voters:
+            if 'voter_id' not in voter or 'name' not in voter:
+                raise Exception('voter should have voter_id and name fields')
+            if voter['voter_id'] in voter_ids or voter['voter_id'] in existing_voters:
+                raise Exception(f"Duplicate voter id: {voter['voter_id']}")
+            voter_ids.append(voter['voter_id'])
+            records.append((poll_id, voter['voter_id'], voter['name']))
+        # save voters
+        cursor.executemany('INSERT INTO poll_voter (poll_id, voter_id, name) VALUES (?, ?, ?)', records)
+        cursor.close()
+        self.conn.commit()
+
+    def register_candidates(self, poll_id, candidates):
+        cursor = self.conn.cursor()
+        # validate candidates
+        candidate_ids = []
+        records = []
+        cursor.execute('SELECT candidate_id FROM poll_candidate WHERE poll_id = ?', (poll_id,))
+        existing_candidates = cursor.fetchall()
+        existing_candidates = [c[0] for c in existing_candidates]
+        for cand in candidates:
+            if 'candidate_id' not in cand or 'name' not in cand:
+                raise Exception('candidate should have candidate_id and name fields')
+            if 'faction' not in cand:
+                cand['faction'] = None
+            if cand['candidate_id'] in candidate_ids or cand['candidate_id'] in existing_candidates:
+                raise Exception(f"Duplicate candidate id: {cand['candidate_id']}")
+            candidate_ids.append(cand['candidate_id'])
+            records.append((poll_id, cand['candidate_id'], cand['name'], cand['faction']))
+        # save voters
+        cursor.executemany('INSERT INTO poll_candidate (poll_id, candidate_id, name, faction) VALUES (?, ?, ?, ?)', records)
+        cursor.close()
+        self.conn.commit()
--- backend/test.py
+++ backend/test.py
@@ -3,6 +3,32 @@ from db.db import Database
 db = Database()
 db.connect()
 
-# db.create_poll('Atharv ka poll', 'referendum', description='Who is balke balke dot blue?')
-p = db.get_poll()
-print(p)
+#db.create_poll('test1', 'runoff', description='Supreme leader of first bench')
+voters = [
+    {'voter_id': 'raman_ojha', 'name': 'Raman Ojha'},
+    {'voter_id': 'aarav_patel', 'name': 'Aarav Patel'},
+    {'voter_id': 'sanya_gupta', 'name': 'Sanya Gupta'},
+    {'voter_id': 'arjun_verma', 'name': 'Arjun Verma'},
+    {'voter_id': 'isha_sharma', 'name': 'Isha Sharma'},
+    {'voter_id': 'vivek_joshi', 'name': 'Vivek Joshi'},
+    {'voter_id': 'kavya_singh', 'name': 'Kavya Singh'},
+    {'voter_id': 'neha_kapoor', 'name': 'Neha Kapoor'},
+    {'voter_id': 'aishwarya_patel', 'name': 'Aishwarya Patel'},
+    {'voter_id': 'kiran_reddy', 'name': 'Kiran Reddy'},
+    {'voter_id': 'riya_kumar', 'name': 'Riya Kumar'},
+    {'voter_id': 'aryan_gupta', 'name': 'Aryan Gupta'},
+    {'voter_id': 'amara_sharma', 'name': 'Amara Sharma'},
+    {'voter_id': 'rohan_kapoor', 'name': 'Rohan Kapoor'},
+    {'voter_id': 'sanjana_gupta', 'name': 'Sanjana Gupta'},
+    {'voter_id': 'ishita_sharma', 'name': 'Ishita Sharma'},
+    {'voter_id': 'aditya_kumar', 'name': 'Aditya Kumar'},
+    {'voter_id': 'aanya_patel', 'name': 'Aanya Patel'},
+    {'voter_id': 'arnav_singh', 'name': 'Arnav Singh'}
+]
+candidates = [
+    {'candidate_id': 'dk', 'name': 'Dave Kenney', 'faction': 'core'},
+    {'candidate_id': 'bd', 'name': 'Bada Dubey', 'faction': 'core'},
+    {'candidate_id': 'tb', 'name': 'Tutle Bajpai', 'faction': 'shexit'}
+]
+# db.register_voters('test1', voters)
+# db.register_candidates('test1', candidates)
