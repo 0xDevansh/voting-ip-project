94155d3ee88cdcf4efdfd7ed62192b30702cceed
DeathVenom54 <fdragon602@gmail.com>
2023-09-10 12:12:39 +0530

    remove destruct_on_switch

--- frontend/App.py
+++ frontend/App.py
@@ -13,15 +13,15 @@ class App(tk.Tk):
     # destruct_on_switch should be True for frames
     # that hold data or are entry forms
     frames_data = {
-        'opening': {'title': 'Main Page', 'frame': OpeningPageFrame, 'destruct_on_switch': False},
-        'credits': {'title': 'Credits', 'frame': CreditsFrame,  'destruct_on_switch': False},
-        'help' : {'title' : 'Help??', 'frame' : Helpframe,  'destruct_on_switch': False},
-        'cre_elec': {'title' : 'Create election' , 'frame' : ElectionDataEntryFrame,  'destruct_on_switch': True},
-        'cand_entry' : {'title' : 'Enter Candidates' , 'frame' : CandidateEntryFrame,  'destruct_on_switch': True},
-        'start_election': {'title': 'Start Election', 'frame': StartElection,  'destruct_on_switch': True},
-        'voting_window' : {'title': 'Voting Window', 'frame': VotingWindow,  'destruct_on_switch': True},
-        'voting_security_check' : {'title': 'Security check', 'frame': VotingSecurityCheck,  'destruct_on_switch': True},
-        'result_page': {'title': 'Result', 'frame': Result,  'destruct_on_switch': True}
+        'opening': {'title': 'Main Page', 'frame': OpeningPageFrame},
+        'credits': {'title': 'Credits', 'frame': CreditsFrame},
+        'help' : {'title' : 'Help??', 'frame' : Helpframe},
+        'cre_elec': {'title' : 'Create election' , 'frame' : ElectionDataEntryFrame},
+        'cand_entry' : {'title' : 'Enter Candidates' , 'frame' : CandidateEntryFrame},
+        'start_election': {'title': 'Start Election', 'frame': StartElection},
+        'voting_window' : {'title': 'Voting Window', 'frame': VotingWindow},
+        'voting_security_check' : {'title': 'Security check', 'frame': VotingSecurityCheck},
+        'result_page': {'title': 'Result', 'frame': Result},
 
     }
 
@@ -32,8 +32,8 @@ class App(tk.Tk):
         # self.geometry('500x450')
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
-        self.loaded_frames = {}
-        self.current_frame = 'opening'
+        self.current_frame_name = 'opening'
+        self.current_frame = None
 
         # show main page
         self.show_frame('opening')
@@ -45,21 +45,15 @@ class App(tk.Tk):
     def show_frame_factory(self, frame_name, context=None):
         def show_function():
             print(f'Showing {frame_name}...')
-            # Remove current frame if it needs to be destructed
-            if App.frames_data[self.current_frame]['destruct_on_switch']:
-                del self.loaded_frames[self.current_frame]
+            # Remove current frame
+            self.current_frame = None
 
-            # Look for existing in loaded_frames
-            if frame_name in self.loaded_frames.keys():
-                frame = self.loaded_frames[frame_name]
-                frame.tkraise()
-            else:
-                frame = App.frames_data[frame_name]['frame'](self, context)
-                frame.grid(row=0, column=0, sticky='news')
-                self.loaded_frames[frame_name] = frame
+            frame = App.frames_data[frame_name]['frame'](self, context)
+            frame.grid(row=0, column=0, sticky='news')
+            self.current_frame = frame
 
             self.title('Poll Pilot - ' + App.frames_data[frame_name]['title'])
-            self.current_frame = frame_name
+            self.current_frame_name = frame_name
         return show_function
 
 def start_application():
