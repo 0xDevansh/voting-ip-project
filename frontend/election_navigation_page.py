53f8262064ddc230d2567c651030f3157777033b
DeathVenom54 <fdragon602@gmail.com>
2023-09-15 00:41:49 +0530

    make navigation functional

--- frontend/election_navigation_page.py
+++ frontend/election_navigation_page.py
@@ -1,14 +1,20 @@
 import tkinter as tk
 import tkinter.messagebox
 import tkinter.ttk as ttk
+
+from backend.db.Database import Database
+
+
 class ElectionNavigationFrame(ttk.Frame):
     def __init__(self, app, context):
         super().__init__(app)
 
         self.frame = tk.Frame(self)
         self.frame.pack()
-        Election_name_list=["Elec_1" ,"Elec_2" ,"Elec_3","Elec_4"]
-        Election_Status_List = ["Not_Started" , "Completed" , 'Running' , 'Not_Started']
+
+        db = Database.get_instance()
+        polls = db.get_poll()
+        print(polls)
        # election_nav_frame_1 = tk.LabelFrame(self.frame)
         #election_nav_frame_1.grid(row=0, column=0, sticky="news")
         election_nav_frame_2 = tk.LabelFrame(self.frame)
@@ -22,24 +28,24 @@ class ElectionNavigationFrame(ttk.Frame):
         Ref_name_label=[]
         Ref_status_Label=[]
         Ref_command_button_1=[]
-        for i in range(len(Election_Status_List)):
+        for i, poll in enumerate(polls):
 
-            Ref_name_label.append(tk.Label(election_nav_frame_2, text=Election_name_list[i]))
-            Ref_status_Label.append(tk.Label(election_nav_frame_2,text=Election_Status_List[i]))
+            Ref_name_label.append(tk.Label(election_nav_frame_2, text=poll['name']))
+            Ref_status_Label.append(tk.Label(election_nav_frame_2,text=poll['status']))
 
 
             Ref_name_label[i].grid(row=i+1, column=0,padx=50)
             Ref_status_Label[i].grid(row=i+1, column=1, padx=50, pady=10)
 
-            if Election_Status_List[i] == 'Not_Started':
-                 Ref_command_button_1.append(tk.Button(election_nav_frame_2,text="Start Election", command= app.show_frame_factory("start_election")))
+            if poll['status'] == 'not_started':
+                 Ref_command_button_1.append(tk.Button(election_nav_frame_2,text="Start Election", command= app.show_frame_factory("start_election", {'poll': poll})))
                  Ref_command_button_1[i].grid(row=i+1, column=2, padx=50, pady=10,sticky='news')
-            elif Election_Status_List[i] == "Running":
-                Ref_command_button_1.append(tk.Button(election_nav_frame_2,text='Add vote/Terminate' , command=app.show_frame_factory('voting_security_check')))
+            elif poll['status'] == "running":
+                Ref_command_button_1.append(tk.Button(election_nav_frame_2,text='Add vote/Terminate' , command=app.show_frame_factory('voting_security_check', {'poll': poll})))
                 Ref_command_button_1[i].grid(row=i+1, column=2, padx=50, pady=10, sticky='news')
-            elif Election_Status_List[i] == 'Completed':
+            elif poll['status'] == 'completed':
                 Ref_command_button_1.append(tk.Button(election_nav_frame_2, text= 'See Result',
-                                                      command=app.show_frame_factory('result_page')))
+                                                      command=app.show_frame_factory('result_page', {'poll': poll})))
                 Ref_command_button_1[i].grid(row=i+1, column=2, padx=50, pady=10,sticky='news')
 
 
--- frontend/opening_page.py
+++ frontend/opening_page.py
@@ -5,8 +5,7 @@ class OpeningPageFrame(ttk.Frame):
     def __init__(self, app, context):
         super().__init__(app)
         self.grid_columnconfigure(0, weight=1)
-        # 6 = no of elements to be centered
-        for i in range(7):
+        for i in range(8):
             self.grid_rowconfigure(i, weight=1)
 
         self.head1 = ttk.Label(self, text="POLL PILOT", font=('Times', 12))
@@ -18,9 +17,11 @@ class OpeningPageFrame(ttk.Frame):
         self.btn1.grid(row=2, column=0)
         self.btn2 = ttk.Button(self, text="Create A Refrendum", command=app.show_frame_factory('create_ref'))
         self.btn2.grid(row=3, column=0)
+        self.btn2 = ttk.Button(self, text="Show Elections", command=app.show_frame_factory('elec_navigation'))
+        self.btn2.grid(row=4, column=0)
         self.btn3 = ttk.Button(self, text="HELP??", command=app.show_frame_factory('help'))
-        self.btn3.grid(row=4, column=0)
+        self.btn3.grid(row=5, column=0)
         self.btn4 = ttk.Button(self, text="Credits", command=app.show_frame_factory('credits'))
-        self.btn4.grid(row=5, column=0)
+        self.btn4.grid(row=6, column=0)
         self.btn5 = ttk.Button(self, text="TESTS", command=app.show_frame_factory('cand_entry'))
-        self.btn5.grid(row=6, column=0)
+        self.btn5.grid(row=7, column=0)
