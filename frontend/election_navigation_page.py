11293ed27f421fdc774dd96c166ce03ba6697471
Atharv <Ramanojha2306@gmail.com>
2023-09-16 15:13:33 +0530

    voting_window refactored

--- frontend/election_navigation_page.py
+++ frontend/election_navigation_page.py
@@ -25,32 +25,32 @@ class ElectionNavigationFrame(ttk.Frame):
             Labels.append(tk.Label(election_nav_frame_2,text=text_for_labels[i]))
             Labels[i].grid(row = 0 , column= i, padx= 50)
 
-        Ref_name_label=[]
-        Ref_type_label=[]
-        Ref_status_Label=[]
+        Elec_name_label=[]
+        Elec_type_label=[] 
+        Elec_status_Label=[] 
 
-        Ref_command_button_1=[]
+        Elec_command_button_1=[]
         for i, poll in enumerate(polls):
 
-            Ref_name_label.append(tk.Label(election_nav_frame_2, text=poll['name']))
-            Ref_type_label.append(tk.Label(election_nav_frame_2, text=poll['type']))
-            Ref_status_Label.append(tk.Label(election_nav_frame_2,text=poll['status']))
+            Elec_name_label.append(tk.Label(election_nav_frame_2, text=poll['name']))
+            Elec_type_label.append(tk.Label(election_nav_frame_2, text=poll['type']))
+            Elec_status_Label.append(tk.Label(election_nav_frame_2,text=poll['status']))
 
 
-            Ref_name_label[i].grid(row=i+1, column=0,padx=50)
-            Ref_type_label[i].grid(row=i+1, column=1,padx=50)
-            Ref_status_Label[i].grid(row=i+1, column=2, padx=50, pady=10)
+            Elec_name_label[i].grid(row=i+1, column=0,padx=50)
+            Elec_type_label[i].grid(row=i+1, column=1,padx=50)
+            Elec_status_Label[i].grid(row=i+1, column=2, padx=50, pady=10)
 
             if poll['status'] == 'not_started':
-                 Ref_command_button_1.append(tk.Button(election_nav_frame_2,text="Start Election", command= app.show_frame_factory("start_election", {'poll': poll})))
-                 Ref_command_button_1[i].grid(row=i+1, column=3, padx=50, pady=10,sticky='news')
+                 Elec_command_button_1.append(tk.Button(election_nav_frame_2,text="Start Election", command= app.show_frame_factory("start_election", {'poll': poll})))
+                 Elec_command_button_1[i].grid(row=i+1, column=3, padx=50, pady=10,sticky='news')
             elif poll['status'] == "running":
-                Ref_command_button_1.append(tk.Button(election_nav_frame_2,text='Add vote/Terminate' , command=app.show_frame_factory('voting_security_check', {'poll': poll})))
-                Ref_command_button_1[i].grid(row=i+1, column=3, padx=50, pady=10, sticky='news')
+                Elec_command_button_1.append(tk.Button(election_nav_frame_2,text='Add vote/Terminate' , command=app.show_frame_factory('voting_security_check', {'poll': poll})))
+                Elec_command_button_1[i].grid(row=i+1, column=3, padx=50, pady=10, sticky='news')
             elif poll['status'] == 'completed':
-                Ref_command_button_1.append(tk.Button(election_nav_frame_2, text= 'See Result',
+                Elec_command_button_1.append(tk.Button(election_nav_frame_2, text= 'See Result',
                                                       command=app.show_frame_factory('result_page', {'poll': poll})))
-                Ref_command_button_1[i].grid(row=i+1, column=3, padx=50, pady=10,sticky='news')
+                Elec_command_button_1[i].grid(row=i+1, column=3, padx=50, pady=10,sticky='news')
 
 
 
