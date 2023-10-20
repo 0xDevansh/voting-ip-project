5d0c536050d86b7fbd21dba7d0ed99bf00e9d3f0
DeathVenom54 <fdragon602@gmail.com>
2023-10-20 22:14:24 +0530

    fix approval logical error

--- backend/approval.py
+++ backend/approval.py
@@ -10,21 +10,21 @@ def calculate_approval(votes, candidates, max_approved=None, min_threshold=None)
         candidate_votes[cand] = 0
 
     # count votes
-    total_votes = 0
     for vote in votes:
         if len(vote) != 0:
             for v in vote:
                 candidate_votes[v] += 1
-                total_votes += 1
     sorted_votes = sorted(candidate_votes.items(), key=lambda x: x[1], reverse=True)
+    candidate_percentages = [(c, v / len(votes) * 100) for (c, v) in candidate_votes.items()]
+
     if not max_approved and not min_threshold:
         # simply return candidate with most votes
         max_votes = max(candidate_votes.values())
         winners = get_keys_with_value(candidate_votes, max_votes)
         return {'winners': winners, 'order': sorted_votes}
     elif min_threshold:
-        # filter winners on having less votes than min_approved
-        winners = [c for (c, v) in sorted_votes if v >= total_votes * min_threshold / 100]
+        # filter winners on having less votes than min_threshold
+        winners = [c for (c, v) in sorted_votes if v >= len(votes) * min_threshold / 100]
         if max_approved and len(winners) > max_approved:
             winners = winners[:max_approved]
         return {'winners': winners, 'order': sorted_votes}
